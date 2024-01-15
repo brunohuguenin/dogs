ae48ae422db3e98bb6e97b43e7a189f2992586b5
Brunohugueninn <brunohugueninn@gmail.com>
2024-01-15 08:29:18 -0300

    context inicial

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
 import SVGDogs from '../Assets/SVGDogs';
+import { UserContext } from '../UserContext';
 
 const Header = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link to="/" className={styles.logo}>
           <SVGDogs />
         </Link>
-        <Link to="/login" className={styles.login}>
-          Login / Criar
-        </Link>
+        {data ? (
+          <Link to="/conta" className={styles.login}>
+            {data.nome}
+          </Link>
+        ) : (
+          <Link to="/login" className={styles.login}>
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
